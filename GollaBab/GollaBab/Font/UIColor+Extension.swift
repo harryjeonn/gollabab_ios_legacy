6e8d5e74595fcf81245dc385fa1887079b2e9bcf
Hoxvi <octxvi@naver.com>
2021-07-12 20:55:44 +0900

    style: Add UIColor

---
+++ GollaBab/GollaBab/Font/UIColor+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor+Extension.swift
+//  GollaBab
+//
+//  Created by 전현성 on 2021/07/12.
+//
+
+import UIKit
+
+extension UIColor {
+    static var bgColor: UIColor {
+        return UIColor(red: 254/255, green: 247/255, blue: 220/255, alpha: 1.0)
+    }
+    
+    static var textColor: UIColor {
+        return UIColor(red: 161/255, green: 152/255, blue: 130/255, alpha: 1.0)
+    }
+    
+    static var lightTextColor: UIColor {
+        return UIColor(red: 194/255, green: 184/255, blue: 163/255, alpha: 1.0)
+    }
+}
