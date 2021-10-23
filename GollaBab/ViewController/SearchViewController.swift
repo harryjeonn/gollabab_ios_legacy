c6dbcd04c78b9ecb75ddfdd313193db5cd39e3e4
Hoxvi <>
2021-10-23 18:18:51 +0900

    [Search] 검색페이지 구현
    검색 페이지 UI 구현
    검색 기능 구현

--- GollaBab/ViewController/SearchViewController.swift
+++ GollaBab/ViewController/SearchViewController.swift
@@ -6,29 +6,58 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class SearchViewController: BaseViewController {
-
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var btnSearch: UIButton!
+    
+    private var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupUI()
+        setupTextField()
+        setupTapEvent()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.title = "검색"
+        textField.text = nil
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupUI() {
+        btnSearch.backgroundColor = .themeColor
+        btnSearch.setTitle("검색", for: .normal)
+        btnSearch.setTitleColor(.whiteColor, for: .normal)
+        btnSearch.layer.cornerRadius = 10
+        
+        textField.textColor = .themeColor
+        textField.font = UIFont(name: "EliceDigitalBaeumOTF", size: 14)
+        textField.placeholder = "검색할 키워드를 입력해주세요."
     }
-    */
-
+    
+    private func setupTextField() {
+        textField.rx
+            .controlEvent([.editingDidEndOnExit])
+            .subscribe(onNext: { _ in
+                
+            }).disposed(by: disposeBag)
+    }
+    
+    private func setupTapEvent() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+        btnSearch.rx.tap
+            .bind {
+                if self.textField.text != nil && self.textField.text != "" {
+                    guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "MapViewController") as? MapViewController else { return }
+                    vc.query = self.textField.text
+                    self.navigationController?.pushViewController(vc, animated: true)
+                }
+            }.disposed(by: disposeBag)
+    }
 }
