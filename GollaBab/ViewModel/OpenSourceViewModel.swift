17081d8a48bfdd526539da68e3865057d1a10be1
hoxvi <octxvi@poliot.co.kr>
2021-12-30 13:37:06 +0900

    Open source license 표시
    FSCalendar license 표시

--- GollaBab/ViewModel/OpenSourceViewModel.swift
+++ GollaBab/ViewModel/OpenSourceViewModel.swift
@@ -28,6 +28,7 @@ enum OpenSourceType {
     case rx
     case alamofire
     case swiftyJSON
+    case fsCalendar
     case font
     
     func title() -> String {
@@ -38,6 +39,8 @@ enum OpenSourceType {
             return "Alamofire"
         case .swiftyJSON:
             return "swiftyJSON"
+        case .fsCalendar:
+            return "FSCalendar"
         case .font:
             return "Font"
         }
@@ -51,6 +54,8 @@ enum OpenSourceType {
             return "Copyright (c) 2014-2021 Alamofire Software Foundation (http://alamofire.org/)\nPermission is hereby granted, free of charge, to any person obtaining a copy\nof this software and associated documentation files (the 'Software'), to deal\nin the Software without restriction, including without limitation the rights\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is\nfurnished to do so, subject to the following conditions:\nThe above copyright notice and this permission notice shall be included in\nall copies or substantial portions of the Software.\nTHE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\nTHE SOFTWARE."
         case .swiftyJSON:
             return "The MIT License (MIT)\n\nCopyright (c) 2017 Ruoyu Fu\nPermission is hereby granted, free of charge, to any person obtaining a copy\nof this software and associated documentation files (the 'Software'), to deal\nin the Software without restriction, including without limitation the rights\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is\nfurnished to do so, subject to the following conditions:\nThe above copyright notice and this permission notice shall be included in\nall copies or substantial portions of the Software.\nTHE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\nTHE SOFTWARE."
+        case .fsCalendar:
+            return "Copyright (c) 2013-2016 FSCalendar (https://github.com/WenchaoD/FSCalendar)\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.\n IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."
         case .font:
             return "EliceDigitalBaeum\n\nCopyright ⓒ 2016 - 2021 elice. All rights reserved"
         }
@@ -67,7 +72,7 @@ class OpenSourceViewModel {
     func loadOpenSourceList() {
         var openSourceList = [SectionOfOpenSource]()
         
-        openSourceList.append(SectionOfOpenSource(header: "", items: [.rx, .alamofire, .swiftyJSON, .font]))
+        openSourceList.append(SectionOfOpenSource(header: "", items: [.rx, .alamofire, .swiftyJSON, .fsCalendar, .font]))
         
         openSourceItems.accept(openSourceList)
     }
